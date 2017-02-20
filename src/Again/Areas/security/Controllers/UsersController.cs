33603050913b6e6f72ff2bd9ac537727415c311f
ccortezawmsu <chriseth1245@gmail.com>
2017-02-20 15:30:39 -0800

    Init

---
+++ src/Again/Areas/security/Controllers/UsersController.cs
@@ -0,0 +1,174 @@
+﻿using Again.Areas.security.Models;
+using Again.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Again.Areas.security.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IList<UserModel>Users
+        { 
+            get
+            {
+                   if (Session["data"]==null)
+                   {
+                       Session["data"] = new List<UserModel>(){
+                           new UserModel {
+                            Id = Guid.NewGuid(),
+                            FirstName = "Christian",
+                            LastName = "Corteza",
+                            Age = 21 ,
+                            Gender =  "Male"
+                        },
+                    new UserModel {
+                        Id = Guid.NewGuid(),
+                        FirstName = "Lorem",
+                        LastName = "Ipsum",
+                        Age = 19 ,
+                        Gender = "Female"
+
+                       }
+                    };
+                   }
+                   return Session["data"] as List<UserModel>;
+
+            }
+        }
+        //
+        // GET: /security/Users/
+        public ActionResult Index()
+        {
+            using (var db = new DataBaseContext())
+            {                
+            var Users = (from user in db.Users
+                             select new UserModel
+                             {
+                                 Id = user.Id,
+                                 FirstName = user.FirstName,
+                                 LastName = user.LastName,
+                                 Age = user.Age,
+                                 Gender = user.Gender
+                             }).ToList();
+              return View(Users);
+            }
+        }
+        //
+        // GET: /security/Users/Details/5
+        public ActionResult Details(Guid id)
+        {
+            var u = Users.FirstOrDefault(users => users.Id == id);
+            // Users.Contains(u);
+            return View(u);
+        }
+
+        //
+        // GET: /security/Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.Gender = new List<SelectListItem> {
+                new SelectListItem
+                {
+                    Value = "Male",
+                    Text = "Male"
+                },
+                new SelectListItem
+                {
+                    Value = "Female",
+                    Text = " Female" 
+                }
+            };
+            return View();
+        }
+
+        //
+        // POST: /security/Users/Create
+        [HttpPost]
+        public ActionResult Create(UserModel Usersmodel)
+        {
+            try
+            {
+             if (ModelState.IsValid == false)
+                    return View();
+             using (var db = new DataBaseContext())
+             {
+                 db.Users.Add(new User
+                 {
+                     Id = Guid.NewGuid(),
+                     FirstName = Usersmodel.FirstName,
+                     LastName = Usersmodel.LastName,
+                     Age = Usersmodel.Age,
+                     Gender = Usersmodel.Gender
+
+                 });
+                 db.SaveChanges();
+             }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /security/Users/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            var u = Users.FirstOrDefault(users => users.Id == id);
+            return View(u);
+        }
+
+        //
+        // POST: /security/Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, UserModel Usersmodel)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var u = Users.FirstOrDefault(user => user.Id == id);
+
+                u.FirstName = Usersmodel.FirstName;
+                u.LastName = Usersmodel.LastName;
+                u.Age = Usersmodel.Age;
+                u.Gender = Usersmodel.Gender;
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /security/Users/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            var u = Users.FirstOrDefault(users => users.Id == id);
+            return View(u);
+        }
+
+        //
+        // POST: /security/Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var u = Users.FirstOrDefault(user => user.Id == id);
+                Users.Remove(u);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/Again/Areas/security/Models/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Again.Areas.security.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+
+        [Required, Display(Name = "FIRST NAME")]
+        [MinLength(2, ErrorMessage = "Minimum of 2 Characters")]
+        [MaxLength(15, ErrorMessage = "Maximum of 15 Characters")]
+        public string FirstName { get; set; }
+
+        [Required, Display(Name = "LAST NAME")]
+        [MinLength(2, ErrorMessage = "Minimum of 2 Characters")]
+        [MaxLength(15, ErrorMessage = "Maximum of 15 Characters")]
+        public String LastName { get; set; }
+
+        [Range(1, 100)]
+        public int? Age { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
